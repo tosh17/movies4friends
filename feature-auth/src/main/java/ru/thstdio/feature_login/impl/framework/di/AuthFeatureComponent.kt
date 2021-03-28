c9068433a3fad80e12b73e6e1362da5ae9b3bbc6
tosh17 <tosh17@list.ru>
2021-03-28 20:50:10 +0300

    add singUp dialog

--- feature-auth/src/main/java/ru/thstdio/feature_login/impl/framework/di/AuthFeatureComponent.kt
+++ feature-auth/src/main/java/ru/thstdio/feature_login/impl/framework/di/AuthFeatureComponent.kt
@@ -3,15 +3,17 @@ package ru.thstdio.feature_login.impl.framework.di
 import dagger.Component
 import ru.thstdio.feature_login.api.AuthFeatureApi
 import ru.thstdio.feature_login.impl.presentation.AuthFragment
+import ru.thstdio.feature_login.impl.presentation.dialogs.sing_up.SingUpDialog
 import ru.thstdio.module_injector.di.PerFeature
 
 @Component(
-    modules = [AuthModule::class,AuthFeatureModule::class],
+    modules = [AuthModule::class, AuthFeatureModule::class],
     dependencies = [AuthFeatureDependencies::class]
 )
 @PerFeature
 abstract class AuthFeatureComponent : AuthFeatureApi {
     abstract fun inject(fragment: AuthFragment)
+    abstract fun inject(dialog: SingUpDialog)
 
     companion object {
         fun initAndGet(dependencies: AuthFeatureDependencies): AuthFeatureComponent {
